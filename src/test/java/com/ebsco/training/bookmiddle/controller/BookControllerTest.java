745d59d2e0f442c2f11cca7921a1bb95b0ced103
Michael Panson <mpanson@kenzan.com>
2017-09-30 14:19:14 -0400

    Added unit and integration tests

---
+++ src/test/java/com/ebsco/training/bookmiddle/controller/BookControllerTest.java
@@ -0,0 +1,104 @@
+package com.ebsco.training.bookmiddle.controller;
+
+import com.ebsco.training.bookmiddle.dto.BookDto;
+import com.ebsco.training.bookmiddle.service.BookService;
+import com.google.common.collect.Lists;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookControllerTest {
+
+    @Autowired
+    private BookController service;
+
+    @MockBean
+    BookService bookService;
+
+    @Before
+    public void setup() {
+        // initialize all the @Mock objects
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void getAll_EntitiesPresent() {
+
+        // Given
+        List<BookDto> expectedBooks = Lists.newArrayList(
+                new BookDto("978-1-4028-9462-6", "Grapes of Wrath", "John Steinbeck", "Fiction"),
+                new BookDto("673-1-4029-8465-3", "One Flew Over the Coockoo's Nest", "Ken Kesay", "Fiction"),
+                new BookDto("453-1-7834-3243-2", "Green Eggs and Ham", "Dr. Seuss", "Childrens"));
+        when(bookService.getBooks()).thenReturn(expectedBooks);
+
+        // When
+        ResponseEntity<List<BookDto>> actualBooks = service.getBooks();
+
+        // Then
+        assertThat(actualBooks, notNullValue());
+        assertThat(actualBooks.getBody(), notNullValue());
+        assertThat(actualBooks.getBody(), hasSize(3));
+    }
+
+    @Test
+    public void getBooks_NoEntitiesPresent() {
+
+        // Given
+        when(bookService.getBooks()).thenReturn(Lists.newArrayList());
+
+        // When
+        ResponseEntity<List<BookDto>> actualBooks = service.getBooks();
+
+        // Then
+        assertThat(actualBooks, notNullValue());
+        assertThat(actualBooks.getBody(), notNullValue());
+        assertThat(actualBooks.getBody(), is(empty()));
+    }
+
+    @Test
+    public void deleteBook_EntityFound() {
+
+        // Given
+        Optional<BookDto> expectedBook = Optional.of(new BookDto("978-1-4028-9462-6", "Grapes of Wrath", "John Steinbeck", "Fiction"));
+        when(bookService.deleteBook(anyString())).thenReturn(expectedBook);
+
+        // When
+        ResponseEntity<Void> response = service.deleteBook("978-1-4028-9462-6");
+
+        // Then
+        assertThat(response, notNullValue());
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+    }
+
+    @Test
+    public void deleteBook_EntityNotFound() {
+
+        // Given
+        Optional<BookDto> expectedBook = Optional.ofNullable(null);
+        when(bookService.deleteBook(anyString())).thenReturn(expectedBook);
+
+        // When
+        ResponseEntity<Void> response = service.deleteBook("978-1-4028-9462-6");
+
+        // Then
+        assertThat(response, notNullValue());
+        assertThat(response.getStatusCode(), is(HttpStatus.NOT_FOUND));
+    }
+}
---
+++ src/test/java/com/ebsco/training/bookmiddle/integrationtest/controller/BookControllerIT.java
@@ -0,0 +1,100 @@
+
+package com.ebsco.training.bookmiddle.integrationtest.controller;
+
+import com.ebsco.training.bookmiddle.dao.BookDao;
+import com.ebsco.training.bookmiddle.dto.BookDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookControllerIT {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private BookDao bookDao;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void getBooks_OK() throws Exception {
+
+        // Given
+        List<BookDto> expectedBooks = Lists.newArrayList(
+                new BookDto("978-1-4028-9462-6", "Grapes of Wrath", "John Steinbeck", "Fiction"),
+                new BookDto("673-1-4029-8465-3", "One Flew Over the Coockoo's Nest", "Ken Kesay", "Fiction"),
+                new BookDto("453-1-7834-3243-2", "Green Eggs and Ham", "Dr. Seuss", "Childrens"));
+        given(bookDao.getBooks()).willReturn(expectedBooks);
+
+        // When
+        ResultActions resultActions = mockMvc.perform(get("/books").accept(MediaType.APPLICATION_JSON));
+
+        // Then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(expectedBooks)));
+    }
+
+    @Test
+    public void getBookById_OK() throws Exception {
+
+        // Given
+        BookDto expectedBook = new BookDto("978-1-4028-9462-6", "Grapes of Wrath", "John Steinbeck", "Fiction");
+        given(bookDao.getBookById(anyString())).willReturn(Optional.of(expectedBook));
+
+        // When
+        ResultActions resultActions = mockMvc.perform(get("/books/1").accept(MediaType.APPLICATION_JSON));
+
+        // Then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(expectedBook)));
+    }
+
+    @Test
+    public void getBookById_NOTFOUND() throws Exception {
+
+        // Given
+        given(bookDao.getBookById(anyString())).willReturn(Optional.ofNullable(null));
+
+        // When
+        ResultActions resultActions = mockMvc.perform(get("/books/1").accept(MediaType.APPLICATION_JSON));
+
+        // Then
+        resultActions
+                .andExpect(status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().string(""));
+    }
+}
